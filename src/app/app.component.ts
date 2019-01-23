c71711415117369fa381648d20a9b0b595c38e92
Séguin Gabriel <1507779@cegepmontpetit.ca>
2019-01-23 17:26:46 -0500

    Test de connection entre api et client angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ClientAngular';
+
+  constructor (public http: HttpClient){}
+
+  getOptions() {
+    return {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Bearer ' + localStorage.getItem('Token')
+      })
+    };
+  }
+
+  testCall(){
+    this.http.get('/api/values').subscribe(r => console.log(r));
+  }
 }
