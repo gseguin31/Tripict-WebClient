81ae33f6cbf2da1c641c2f79e51b6a5e4b23c779
Bélance Antoine <1447500@cegepmontpetit.ca>
2019-02-05 10:23:49 -0500

    Fin angular pour le moment?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {TranslateService} from '@ngx-translate/core';
+import {conditionallyCreateMapObjectLiteral} from '@angular/compiler/src/render3/view/util';
+import {compareLogSummaries} from '@angular/core/src/render3/styling/class_and_style_bindings';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +14,7 @@ export class AppComponent {
 
   constructor (public http: HttpClient, private translate: TranslateService){
     translate.setDefaultLang('fr');
+    this.translate.use('fr');
   }
 
   getOptions() {
@@ -25,5 +28,15 @@ export class AppComponent {
 
   testCall(){
     this.http.get('/api/values').subscribe(r => console.log(r));
+  }
+
+  switchLanguage() {
+    if (this.translate.currentLang === 'fr'){
+      this.translate.use('en');
+    }
+    else {
+      this.translate.use('fr');
+    }
+    console.log(this.translate.currentLang);
   }
 }
